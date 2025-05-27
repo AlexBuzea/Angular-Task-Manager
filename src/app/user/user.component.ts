6fe1aaa49a8fb72894d18bbcdf82511c5da1855c
Alex Buzea <alexbuzea@ymail.com>
2025-05-27 17:43:11 +0100

    adding type alias instead of inline types for user component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,10 @@
 import { Component, Input, Output, EventEmitter, output } from '@angular/core';
+
+type User = {
+  id: string;
+  avatar: string;
+  name: string;
+};
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -10,11 +16,7 @@ export class UserComponent {
   // @Input({ required: true }) avatar!: string;
   // @Input({ required: true }) name!: string;
 
-  @Input({ required: true }) user!: {
-    id: string;
-    avatar: string;
-    name: string;
-  };
+  @Input({ required: true }) user!: User;
   @Output() select = new EventEmitter<string>();
   // select = output<string>();
 
