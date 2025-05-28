9d530495f681fe21f4eab16fbc6163fea45c0254
Alex Buzea <alexbuzea@ymail.com>
2025-05-28 11:30:41 +0100

    adding conditional css for selected user

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -7,13 +7,9 @@ import type { User } from './user.model';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  // @Input({ required: true }) id!: string;
-  // @Input({ required: true }) avatar!: string;
-  // @Input({ required: true }) name!: string;
-
   @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
   @Output() select = new EventEmitter<string>();
-  // select = output<string>();
 
   get imagePath() {
     return 'assets/users/' + this.user.avatar;
